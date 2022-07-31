0285a8d46b7d667ca27b15b81b4321c58a3f69d1
Shulaev <mail@ntdservice.ru>
2022-07-31 14:55:44 +0300

    optimize citilink

--- goods_parser/src/main.py
+++ goods_parser/src/main.py
@@ -1,14 +1,16 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 from tqdm.auto import trange
 from  pprint import pprint
+import csv
 
 
 
 class CitilinkParser():
 
-    def __init__(self):
-        self.price_filter  = 40000
+    def __init__(self, key:str, budget):
+        self.price_filter  = budget
         self.headers = {
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
         }
@@ -38,25 +40,34 @@ class CitilinkParser():
             '-tv': 'https://www.citilink.ru/catalog/televizory/'
         }
 
-        self.result_obj =  dict()
-        self.result_file = 'result.txt'
-
-
-
-    def get_content(self, key:str, pages:int=1):
-
         self.url = ''
         self.user_key = key
 
         for k, v in self.urls.items():
             if k == self.user_key:
-                url = self.urls[k]
+                self.url = self.urls[k]
 
+        self.result_obj =  dict()
+        self.result_file = 'result.txt'
+
+    def get_max_pages(self):
+
+        r = requests.get(self.url+f'?p={1}', headers=self.headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        self.max_pages = soup.find_all('a', class_='PaginationWidget__page-link')
+        max_num = max([int(link.get('data-page')) for link in self.max_pages])
+
+        return max_num
+
+
+    def get_content(self, pages:int=1):
+  
         self.pages = pages
         tumbler = True
 
         for num in trange(1, pages+1):
-            r = requests.get(url+f'?p={num}', headers=self.headers)
+            r = requests.get(self.url+f'?p={num}', headers=self.headers)
+            current_url = r.request.url.replace(f'?p={num}', '')
             soup = BeautifulSoup(r.text, 'html.parser')
             catalog = soup.find_all('div', class_='ProductCardVerticalLayout')
 
@@ -68,10 +79,12 @@ class CitilinkParser():
                 
                 if tumbler:
                     good = item.find('a', class_='ProductCardVertical__name').get('title')
+                    link = item.find('a', class_='ProductCardVertical__name').get('href')
                     price_str = item.find('span', class_='ProductCardVerticalPrice__price-current_current-price')
     
                 else:
                     good = item.find('a', class_='ProductCardHorizontal__title').get('title')
+                    link = item.find('a', class_='ProductCardHorizontal__title').get('href')
                     price_str = item.find('span', class_='ProductCardHorizontal__price_current-price')
 
                 if price_str == None:
@@ -79,17 +92,25 @@ class CitilinkParser():
                 else:
                     price = ''.join(price_str.text.split())
         
-                self.result_obj[good] = price
+                self.result_obj[good] = {good: price, 'link': 'https://www.citilink.ru/'+link}
+
 
     def write_content(self):
-        with open(self.result_file, 'r+') as f:
+
+        if os.path.exists('result.txt'):
+            self.f_param = 'a'
+        else:
+            self.f_param = 'w+'
+
+        with open(self.result_file, self.f_param) as f:
+
             for item, price in self.result_obj.items():
-                if int(price) > self.price_filter:
+
+                if int(price[item]) > self.price_filter:
                     continue
                 else:
-                    line = f'{item} : Цена {price} рублей'
+                    line = f'{item} | Цена {price[item]} рублей | {price["link"]}'
                     f.write(f'{line}\n')
-
 
 
 if __name__ == '__main__':
@@ -97,8 +118,6 @@ if __name__ == '__main__':
     while True:
         print(
         """
-            start parser
-
             Команды для поиска:
 
             -n  ноутбуки
@@ -125,9 +144,17 @@ if __name__ == '__main__':
         )
         print('Ожидание ввода:')
         ans_key = input()
-                
-        parser = CitilinkParser()
-        parser.get_content(ans_key)
+
+        print('Введите бюджет покупки')
+        budget = int(input())
+
+        parser = CitilinkParser(ans_key, budget)
+        max_pages = parser.get_max_pages()
+
+        print(f'найдено {max_pages} страниц товаров. Введите глубину поиска от 1 до {max_pages}')
+        ans_page = int(input())
+
+        parser.get_content(ans_page)
         parser.write_content()
 
    
